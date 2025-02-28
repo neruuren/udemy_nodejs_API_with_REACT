e814859701ff26f35f90232e51c8f9d0ab593953
gregoryDAY <gregory.day@etik.com>
2025-02-28 14:06:38 +0100

    transform react version code in singlePost page + change url fetched

--- src/pages/Feed/SinglePost/SinglePost.jsx
+++ src/pages/Feed/SinglePost/SinglePost.jsx
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Image from '../../../components/Image/Image';
 import './SinglePost.css';
 
-class SinglePost extends Component {
-  state = {
+const SinglePost = () => {
+  const { postId } = useParams(); // Récupère l'ID depuis l'URL
+  const [post, setPost] = useState({
     title: '',
     author: '',
     date: '',
     image: '',
     content: ''
-  };
+  });
 
-  componentDidMount() {
-    const postId = this.props.match.params.postId;
-    fetch('URL')
+  useEffect(() => {
+    fetch(`http://localhost:8080/feed/post/${postId}`)
       .then(res => {
         if (res.status !== 200) {
-          throw new Error('Failed to fetch status');
+          throw new Error('Failed to fetch post');
         }
         return res.json();
       })
       .then(resData => {
-        this.setState({
+        setPost({
           title: resData.post.title,
           author: resData.post.creator.name,
+          image: 'http://localhost:8080/' + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
         });
       })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+      .catch(err => console.error(err));
+  }, [postId]);
 
-  render() {
-    return (
-      <section className="single-post">
-        <h1>{this.state.title}</h1>
-        <h2>
-          Created by {this.state.author} on {this.state.date}
-        </h2>
-        <div className="single-post__image">
-          <Image contain imageUrl={this.state.image} />
-        </div>
-        <p>{this.state.content}</p>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="single-post">
+      <h1>{post.title}</h1>
+      <h2>Created by {post.author} on {post.date}</h2>
+      <div className="single-post__image">
+        <Image contain imageUrl={post.image} />
+      </div>
+      <p>{post.content}</p>
+    </section>
+  );
+};
 
 export default SinglePost;
